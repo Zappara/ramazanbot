b10f5a4b804cb897edf5cab7b10fd6eda70b257c
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-13 13:05:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,7 @@ function setActivity() {
     var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)]; //Random Math to set the setGame to something in the GameInfo array
 
     client.user.setActivity(info) // "playing Game" '...' Sets the setGame to what the info Random math picked from the GameInfo Array
-    if (debugMode === "1") {
-        console.log(`[ OYNUYOR ] Zappara şu anda ( ${info} ) oynuyor`) //Logs to console what the setGame was set as.
+    console.log(`[ OYNUYOR ] Ramazan BOT şu anda ( ${info} ) oynuyor`) //Logs to console what the setGame was set as.
     }
 
 }
