d19a29b71065b5f16d5de28d5a7f41a1e73631a2
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-19 00:21:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
-const yt = require("ytdl-core");
+const ytdl = require("ytdl-core");
 const client = new Discord.Client();
 var prefix = "+";
+
+const connections = new Map();
+let broadcast;
+
 
 //TOKEN
 client.login(process.env.BOT_TOKEN);
@@ -19,9 +23,7 @@ client.on("ready", () => {
   client.user.setStatus("STREAMING");
   client.user.setActivity(
     "+yardım | RamazanBot | Hoş Geldin Ey Şehri Ramazan |www.enesonurata.cf",
-    {
-      type: "STREAMING"
-    }
+    {type: "STREAMING"}
   );
 });
 
@@ -183,3 +185,66 @@ client.on("message", msg => {
   }
 });
 
+
+
+
+client.on('message', m => {
+  if (!m.guild) return;
+  if (m.content.startsWith('+gir')) {
+    const channel = m.guild.channels.get(m.content.split(' ')[1]) || m.member.voiceChannel;
+    if (channel && channel.type === 'voice') {
+      channel.join().then(conn => {
+        conn.player.on('error', (...e) => console.log('player', ...e));
+        if (!connections.has(m.guild.id)) connections.set(m.guild.id, { conn, queue: [] });
+        m.reply('Girdim!');
+      });
+    } else {
+      m.reply('Lütfen bir sesli kanala giriniz!');
+    }
+  } else if (m.content.startsWith('+çal')) {
+    if (connections.has(m.guild.id)) {
+      const connData = connections.get(m.guild.id);
+      const queue = connData.queue;
+      const url = m.content.split(' ').slice(1).join(' ')
+        .replace(/</g, '')
+        .replace(/>/g, '');
+      queue.push({ url, m });
+      if (queue.length > 1) {
+        m.reply(`İstediğiniz müzik ${queue.length - 1} adet müzikten sonra çalacak`);
+        return;
+      }
+      doQueue(connData);
+    }
+  } else if (m.content.startsWith('+geç')) {
+    if (connections.has(m.guild.id)) {
+      const connData = connections.get(m.guild.id);
+      if (connData.dispatcher) {
+        connData.dispatcher.end();
+      }
+    }
+  } else if (m.content.startsWith('+kuyruk')) {
+    if (connections.has(m.guild.id)) {
+      const connData = connections.get(m.guild.id);
+      const queue = connData.queue;
+      m.reply(queue.map(q => q.url));
+    }
+  }
+});
+
+function doQueue(connData) {
+  const conn = connData.conn;
+  const queue = connData.queue;
+  const item = queue[0];
+  if (!item) return;
+  const stream = ytdl(item.url, { filter: 'audioonly' }, { passes: 3 });
+  const dispatcher = conn.playStream(stream);
+  stream.on('info', info => {
+    item.m.reply(`Çalınan: **${info.title}**`);
+  });
+  dispatcher.on('end', () => {
+    queue.shift();
+    doQueue(connData);
+  });
+  dispatcher.on('error', (...e) => console.log('dispatcher', ...e));
+  connData.dispatcher = dispatcher;
+}
