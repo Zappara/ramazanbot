c094140ca22626bc596f674df4b055782bf96ef5
Enes Onur Ata <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-19 01:56:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -187,72 +187,23 @@ client.on("message", msg => {
   }
 });
 
-
+//Zappara
 client.on("message", msg => {
   if (msg.content === prefix + "zappara") {
 const embed = new Discord.RichEmbed()
   .setTitle("This is your title, it can hold 256 characters")
   .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
   .setColor(0x00AE86)
   .setDescription("This is the main body of text, it can hold 2048 characters.")
   .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
   .setImage("http://i.imgur.com/yVpymuV.png")
   .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
   .setTimestamp()
   .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
   .addField("This is a field title, it can hold 256 characters",
     "This is a field value, it can hold 2048 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
   .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
   .addBlankField(true)
   .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-  message.channel.send({embed});
-
-
-
-
-
-var youtubedl = require('youtube-dl');
-var fs = require('fs');
-var bot = new Discord.Client();
-
-bot.on('message', function (message) {
-    if (message.content.indexOf('!') === 0) {
-        var args = message.content.split(' ');
-        
-        if (args[0] === '+ramazan yardım') {
-            bot.sendMessage(message.channel, 'A help page will be availiable soon.');
-        } else if (args[0] === '!summon') {
-            if (message.author.voiceChannel == null) {
-                bot.sendMessage(message.channel, 'Sesli kanala katılmalısın!');
-            } else {
-                bot.joinVoiceChannel(message.author.voiceChannel);
-                bot.sendMessage(message.channel, 'Kanala Katıldım.');
-            }
-        } else if (args[0] === '+çal') {
-            if (args[1] == null) {
-                bot.sendMessage(message.channel, 'Kullanımı: !çal <url>');
-                return;
-            }
-            
-            var video = youtubedl(args[1]);
-            video.on('info', function(info) {
-                if (!fs.exists("./audio_cache/" + info.id + ".mp3")) {
-                    video.pipe(fs.createWriteStream("./audio_cache/" + info.id + ".mp3"));
-                }
-                //bot.voiceConnection.playFile("./audio_cache/" + info.id + ".mp3");
-            });
-        }
-    }
-});
+  message.channel.send({embed})
+  }});
